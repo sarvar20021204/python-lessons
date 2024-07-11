dff65326a165dbffc9502fd7bcb2455c1a2c0ced
sarvar20021204 <sarvaravloqulov803@gmail.com>
2024-07-11 02:46:33 -0400

    for ucun birinchi kodim
    for ucun birinchi kodim endi urgandim

---
+++ for.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 00:23:14 2024
+
+@author: sarvar
+"""
+
+print("HELLO WORLD")
+
+#row=int(input("yugurishlar sonini kiriting::"))
+
+#for i in range(1,row+1,1):
+    
+#    for j in range(1,i+1):
+        
+ #       print(j,end=' ')
+
+ #   print("")
+ 
+#son=int(input('sonni kiriting:'))
+#s=0
+#for i in range(1,son+1,1):
+  #  s+=i
+#print(s)
+
+#son=int(input('sonni kiriting:'))
+
+#for i in range (1,11,1):
+ #   j=i*son
+  #  print(j)
+
+
+#row=int(input('sonni kiriitng::'))
+#for i in range(1,row+1,1):
+#    for j in range(row-i+1,0,-1):
+ #       print(j,end=" ")
+ #   print("")
+ 
+#for i in range(-10,0,+1):
+#    print(i)
+
+
+son1=int(input('birinchi sonni kiriitng::'))
+son2=int(input('ikkinchi sonni kiriting::'))
+
+for i in range(son1,son2+1,1):
+    
